edca1390ba60967e5d750a09fc76a9ba3dc22074
Francisco Sosa <frsosao@gmail.com>
2021-04-12 00:18:55 -0600

    create a basic componetns

--- vue_todo_app/src/App.vue
+++ vue_todo_app/src/App.vue
@@ -1,17 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Search from "./components/Search";
+import TodoAdd from "./components/TodoAdd";
+import Todos from "./components/Todos";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Search,
+    Todos,
+    TodoAdd,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 0,
+          title: "Terminar ticket",
+          complete: false,
+        },
+        {
+          id: 0,
+          title: "Terminar algo1",
+          complete: false,
+        },
+        {
+          id: 0,
+          title: "Terminar algo2",
+          complete: false,
+        }
+      ],
+      copyTodos: []
+    };
+  },
+};
 </script>
 
 <style>
